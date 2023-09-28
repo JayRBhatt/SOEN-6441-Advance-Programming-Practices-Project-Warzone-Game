b6c173a96f4db9d2592a5a0e5dbe721e8f927187
Meera27 <meeramnair99@gmail.com>
2023-09-28 17:35:11 -0400

    Player model class added

---
+++ src/main/java/model/Player.java
@@ -0,0 +1,26 @@
+package model;
+
+public class Player {
+
+	private int d_PlayerId;
+	
+	private String d_PlayerName;
+
+	public int getD_PlayerId() {
+		return d_PlayerId;
+	}
+
+	public void set_PlayerId(int p_PlayerId) {
+		this.d_PlayerId = p_PlayerId;
+	}
+
+	public String get_PlayerName() {
+		return d_PlayerName;
+	}
+
+	public void set_PlayerName(String p_PlayerName) {
+		this.d_PlayerName = p_PlayerName;
+	}
+	
+
+}
