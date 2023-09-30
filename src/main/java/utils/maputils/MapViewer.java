b215803ed1b168d3f8c1614629a4f1d9debe290b
Madhav Anadkat <madhavanadkat67@gmail.com>
2023-09-30 15:51:32 -0400

    MapEditor improvement commit 6

--- src/main/java/utils/maputils/MapViewer.java
+++ src/main/java/utils/maputils/MapViewer.java
@@ -1,10 +1,22 @@
 package utils.maputils;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import model.GameMap;
+import utils.InvalidCommandException;
 
 public class MapViewer 
 {
-    public static void readMap() throws ValidationException
+    public static void readMap(GameMap p_GameMap, String p_FileName) throws InvalidCommandException
     {
         try
         {
@@ -16,32 +28,32 @@ public class MapViewer
             BufferedReader l_Buffer = new BufferedReader(l_FileReader);
             while(l_Buffer.ready())
             {
-                String l_read = l_Buffer.readLine();
+                String l_Read = l_Buffer.readLine();
                 if (!l_Read.isEmpty()) 
                 {
                     if (l_Read.contains("[") && l_Read.contains("]")) 
                     {
                         l_CurrentKey = l_Read.replace("[", "").replace("]", "");
-                        l_MapFileContents.put(l_CurrentKey, new ArrayList<>());
+                        l_FileContent.put(l_CurrentKey, new ArrayList<>());
                     } 
                     else 
                     {
-                        l_MapFileContents.get(l_CurrentKey).add(l_Read);
+                        l_FileContent.get(l_CurrentKey).add(l_Read);
                     }
                 }
             }
 
-            readContinentsFromFile(p_GameMap, l_MapFileContents.get("Continents"));
-            Map<String, List<String>> l_CountryNeighbors = readCountriesFromFile(p_GameMap, l_MapFileContents.get("Territories"));
+            readContinentsFromFile(p_GameMap, l_FileContent.get("Continents"));
+            Map<String, List<String>> l_CountryNeighbors = readCountriesFromFile(p_GameMap, l_FileContent.get("Territories"));
             addNeighborsFromFile(p_GameMap, l_CountryNeighbors); 
         }
-        catch (ValidationException | IOException e) 
+        catch (InvalidCommandException | IOException e) 
         {
-            throw new ValidationException(e.getMessage());
+            throw new InvalidCommandException(e.getMessage());
         }
     }
 
-    public static void readContinentsFromFile(GameMap p_GameMap, List<String> p_ContinentArray) throws ValidationException 
+    public static void readContinentsFromFile(GameMap p_GameMap, List<String> p_ContinentArray) throws InvalidCommandException 
     {
         for (String l_InputString : p_ContinentArray) 
         {
@@ -53,7 +65,7 @@ public class MapViewer
         }
     }
 
-    public static Map<String, List<String>> readCountriesFromFile(GameMap p_GameMap, List<String> p_CountryArray) throws ValidationException 
+    public static Map<String, List<String>> readCountriesFromFile(GameMap p_GameMap, List<String> p_CountryArray) throws InvalidCommandException
     {
         Map<String,List<String>> l_NeighborCountries = new HashMap<>();
         for(String l_CountryString : p_CountryArray)
@@ -62,13 +74,13 @@ public class MapViewer
             if(l_CountryStringArray.size() >= 2)
             {
                 p_GameMap.addCountry(l_CountryStringArray.get(0), l_CountryStringArray.get(1));
-                l_NeighborCountries.put(l_CountryStringArray.get(0), l_CountryStringArray.subList(2, l_CountryStringArray.size()))
+                l_NeighborCountries.put(l_CountryStringArray.get(0), l_CountryStringArray.subList(2, l_CountryStringArray.size()));
             }
         }
         return l_NeighborCountries;
     }
 
-    public static void addNeighborsFromFile(GameMap p_GameMap, Map<String, List<String>> p_NeighborList) throws ValidationException {
+    public static void addNeighborsFromFile(GameMap p_GameMap, Map<String, List<String>> p_NeighborList) throws InvalidCommandException {
         for (String l_Country : p_NeighborList.keySet()) {
             for (String l_Neighbor : p_NeighborList.get(l_Country)) {
                 p_GameMap.addNeighbor(l_Country, l_Neighbor);
