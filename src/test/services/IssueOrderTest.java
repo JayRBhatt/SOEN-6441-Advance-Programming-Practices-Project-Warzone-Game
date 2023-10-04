99fd16b7f221874cfeb9dc326153d7563875928b
MariyaBosy <mariabosy@gmail.com>
2023-10-04 18:17:34 -0400

    issueorderTest

--- src/test/services/IssueOrderTest.java
+++ src/test/services/IssueOrderTest.java
@@ -1,66 +1,114 @@
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
+
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
-import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class IssueOrderTest {
-    private IssueOrder issueOrderTest;
-    private String playerName;
-    private int noOfReinforcementArmies;
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+public class OrderIssueTest {
 
-    public IssueOrderTest(String playerName)
-    {
-        this.playerName = playerName;
+    private OrderIssue orderIssue;
+    private GameMap gameMap;
+    private Player player1;
+    private Player player2;
+    private Country country1;
+    private Country country2;
+    private Order order1;
+    private Order order2;
+
+    // Helper method to provide input for simulating player input
+    private void provideInput(String data) {
+        InputStream stdin = System.in;
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
     }
-    public IssueOrderTest(int noOfReinforcementArmies)
-    {
-        this.noOfReinforcementArmies = noOfReinforcementArmies;
-    }
-    
+
     @Before
     public void setUp() {
-        issueOrderTest = new issueOrderTest(); // Create an instance of your class
-        System.setOut(new PrintStream(outContent));
-        System.setErr(new PrintStream(errContent));
+        orderIssue = new OrderIssue();
+        gameMap = GameMap.getInstance();
+
+        player1 = new Player();
+        player1.setPlayerName("Mariya");
+        player2 = new Player();
+        player2.setPlayerName("Reema");
+
+        country1 = new Country();
+        country1.setCountryName("India");
+        country1.setPlayer(player1);
+
+        country2 = new Country();
+        country2.setCountryName("China");
+        country2.setPlayer(player2);
+
+        player1.getOccupiedCountries().add(country1);
+        player2.getOccupiedCountries().add(country2);
+
+        // Create mock orders
+        order1 = mock(Order.class);
+        when(order1.execute()).thenReturn(true);
+        OrderDetails orderDetails1 = new OrderDetails(player1, "India", 3);
+        when(order1.getOrderDetails()).thenReturn(orderDetails1);
+
+        order2 = mock(Order.class);
+        when(order2.execute()).thenReturn(true);
+        OrderDetails orderDetails2 = new OrderDetails(player2, "China", 2);
+        when(order2.getOrderDetails()).thenReturn(orderDetails2);
+
+        player1.getOrders().add(order1);
+        player2.getOrders().add(order2);
+        gameMap.getGamePlayers().put("Player1", player1);
+        gameMap.getGamePlayers().put("Player2", player2);
     }
 
     @Test
     public void testBegin() {
-        issueOrder issueOrderObj = new issueOrder();
-        issueOrderObj.playerName =this.playerName;
-        issueOrderObj.noOfReinforcementArmies=this.noOfReinforcementArmies;
-        // Prepare your test data and dependencies, including the game map and players
-        Map<Integer, Player> gamePlayers = new HashMap<>();
-        // Populate gamePlayers with test player objects
-        
-        // Set the game map's gamePlayers using reflection or other appropriate means
-        // issueOrderTest.setGamePlayers(gamePlayers);
+        // Simulate player input
+        provideInput("deploy India 3\n");
 
-        // Mock user input for readFromPlayer method
-        String mockInput = "deploy India 10 \n";
-        InputStream stdin = System.in;
-        System.setIn(new ByteArrayInputStream(mockInput.getBytes()));
+        // Capture the output for validation
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
 
+        // Call the method
         try {
-            issueOrderTest.begin(1); // Call the method being tested
+            orderIssue.begin(1);
         } catch (InvalidCommandException e) {
-            fail("An exception occurred: " + e.getMessage());
+            e.printStackTrace();
         }
 
-        // Perform assertions on the output using outContent
-        String expectedOutput = "The Armies assigned to "+playerName +" are :"+ noOfReinforcementArmies
-                                + "Please assign your armies only to the below listed countries:\n";
-        assertTrue(outContent.toString().contains(expectedOutput));
+        // Validate the output here based on your expectations
+        String expectedOutput = "The Armies assigned to Player Player1 are : 3\n" +
+                "Please assign your armies only to the below listed countries:\n" +
+                "India\n" +
+                "****************************************************************************************\n" +
+                "Lets Begin Issuing Orders ! : \n" +
+                "1.For any guidance type help we are happy to help you.\n" +
+                "You have assigned all your armies to the countries.Lets Move to the next phase!!\n" +
+                "**************************************************************************************\n";
+        assertEquals(expectedOutput, outputStream.toString());
+    }
 
-        // Restore standard input and output
-        System.setIn(stdin);
-        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+    @Test
+    public void testVerifyCommandDeployValid() {
+        assertTrue(orderIssue.VerifyCommandDeploy("DEPLOY India 3"));
+    }
+
+    @Test
+    public void testVerifyCommandDeployInvalid() {
+        assertFalse(orderIssue.VerifyCommandDeploy("INVALID India 3"));
+    }
+
+    @After
+    public void tearDown() {
+        System.setIn(System.in);
+        System.setOut(System.out);
     }
 }
