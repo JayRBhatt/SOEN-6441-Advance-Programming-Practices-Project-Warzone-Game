07f8236352c2207b6826601c4dc292b0f5bd7004
JayRBhatt <60239577+JayRBhatt@users.noreply.github.com>
2023-11-05 14:11:10 -0500

    Log creation files commit

---
+++ src/main/java/utils/loggers/LogEntryBuffer.java
@@ -0,0 +1,55 @@
+package utils.logger;
+
+import utils.Observable;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
+/**
+ * This class records all actions within the game as observable events.
+ * It enables observers to be notified of game actions.
+ */
+public class LogEntryBuffer implements Observable {
+    /**
+     * Instance of {@code LogEntryWriter} to handle log writing.
+     */
+    private LogEntryWriter d_logEntryWriter = new LogEntryWriter(); // Observer instance for logging.
+
+    /**
+     * Receives game action information and notifies the observer.
+     *
+     * @param p_actionInfo The information about the game action to log.
+     */
+    public void logAction(String p_actionInfo) {
+        notifyObservers(p_actionInfo);
+    }
+
+    /**
+     * Notifies the observer with the given message.
+     *
+     * @param p_message The message to notify the observer with.
+     */
+    public void notifyObservers(String p_message) {
+        d_logEntryWriter.writeLogEntry(p_message);
+    }
+
+    /**
+     * Prepares a new log file by clearing any previous content.
+     * This is typically used before the start of a new game.
+     */
+    public void prepareNewLogFile() {
+        PrintWriter l_writer = null;
+        String l_fileName = "game_log";
+        try {
+            l_writer = new PrintWriter(new BufferedWriter(new FileWriter("logFiles/" + l_fileName + ".log", false)));
+            l_writer.print(""); // Clear the file content.
+        } catch (Exception ex) {
+            // Exception handling logic should be here
+        } finally {
+            if (l_writer != null) {
+                l_writer.close(); // Close the writer to prevent resource leak.
+            }
+        }
+    }
+}
---
+++ src/main/java/utils/loggers/LogEntryWriter.java
@@ -0,0 +1,42 @@
+package utils.logger;
+
+import utils.Observer;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
+/**
+ * Observes LogEntryBuffer and records the actions to a log file.
+ */
+public class LogEntryWriter implements Observer {
+
+    /**
+     * Receives updates from the observed subject and logs the information.
+     *
+     * @param p_message the information to be logged.
+     */
+    public void receiveUpdate(String p_message) {
+        logMessage(p_message);
+    }
+
+    /**
+     * Logs game actions to a specified log file.
+     *
+     * @param p_logMessage the content to be logged.
+     */
+    public void logMessage(String p_logMessage) {
+        PrintWriter l_writer = null;
+        String l_fileName = "demo";
+        try {
+            l_writer = new PrintWriter(new BufferedWriter(new FileWriter("logFiles/" + l_fileName + ".log", true)));
+            l_writer.println(p_logMessage);
+        } catch (Exception l_exception) {
+            System.out.println(l_exception.getMessage());
+        } finally {
+            if (l_writer != null) {
+                l_writer.close();
+            }
+        }
+    }
+}
