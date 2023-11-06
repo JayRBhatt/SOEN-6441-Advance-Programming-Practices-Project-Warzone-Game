c2fc85a899a8290bf37c97277178a0931666ce03
Bhargav6100 <bhargavfofandi314@gmail.com>
2023-11-06 15:22:45 -0500

    Order test cases done

--- src/test/java/model/order/AirliftOrderTest.java
+++ src/test/java/model/order/AirliftOrderTest.java
@@ -1,5 +1,106 @@
 package model.order;
 
+import services.OrderIssue;
+import model.Cards;
+import model.CardsType;
+import model.Country;
+import model.GameMap;
+import model.Player;
+import model.orders.Order;
+import model.orders.OrderCreator;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class tests the Airlift Order
+ * @author Surya Manian
+ */
 public class AirliftOrderTest {
-    
+    GameMap d_GameMap;
+    List<Country> l_CountryList1 = new ArrayList<Country>();
+    List<Country> l_CountryList2 = new ArrayList<Country>();
+
+    /**
+     * Setup for the test case
+     * @throws Exception in case of any exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        d_GameMap = GameMap.getInstance();
+        d_GameMap.addGamePlayer("Player1");
+        d_GameMap.addGamePlayer("Player2");
+        d_GameMap.addContinent("Asia", "5");
+        d_GameMap.addCountry("India", "Asia");
+        d_GameMap.addCountry("Pakistan", "Asia");
+        d_GameMap.addCountry("SriLanka", "Asia");
+        d_GameMap.addCountry("Afganisthan", "Asia");
+        d_GameMap.addCountry("Bangladesh", "Asia");
+        d_GameMap.addCountry("Myanmar", "Asia");
+        d_GameMap.addCountry("China", "Asia");
+        d_GameMap.assignCountries();
+        l_CountryList1 = d_GameMap.getGamePlayer("Player1").getOccupiedCountries();
+        l_CountryList2 = d_GameMap.getGamePlayer("Player2").getOccupiedCountries();
+    }
+
+    /**
+     * Clear the instance
+     *
+     * @throws Exception in case of any exception
+     */
+    @After
+    public void tearDown() throws Exception {
+        d_GameMap.ClearMap();
+    }
+
+    /**
+     * Test to check if the command executes correctly
+     *
+     */
+    @Test
+    public void execute() {
+        Player l_Player = d_GameMap.getGamePlayer("Player1");
+        l_Player.addPlayerCard(new Cards(CardsType.AIRLIFT));
+        l_CountryList1.get(0).setArmies(100);
+        OrderIssue.Commands = "airlift " + l_CountryList1.get(0).getCountryName() + " " + l_CountryList1.get(1).getCountryName()+ " "+ 10;
+        Order l_Order1 = OrderCreator.CreateOrder(OrderIssue.Commands.split(" "), l_Player);
+        l_Player.receiveOrder(l_Order1);
+        assertTrue(l_Player.nextOrder().execute());
+    }
+
+    /**
+     * Test the validation of Airlift command for when the target country belongs to same player
+     */
+    @Test
+    public void checkIfCommandIsTrue() {
+        Player l_Player = d_GameMap.getGamePlayer("Player1");
+        l_Player.addPlayerCard(new Cards(CardsType.AIRLIFT));
+        l_CountryList1.get(0).setArmies(100);
+        System.out.println("Source: "+ l_CountryList1.get(0).getArmies());
+        OrderIssue.Commands = "airlift " + l_CountryList1.get(0).getCountryName() + " " + l_CountryList1.get(1).getCountryName()+ " "+ 10;
+        Order l_Order1 = OrderCreator.CreateOrder(OrderIssue.Commands.split(" "), l_Player);
+        l_Player.receiveOrder(l_Order1);
+        assertTrue(l_Player.nextOrder().validateCommand());
+    }
+
+    /**
+     * Test the validation of Airlift command when the target country does not belong to player
+     *
+     */
+    @Test
+    public void checkIfCommandIsfalse() {
+        Player l_Player1 = d_GameMap.getGamePlayer("Player1");
+        l_Player1.addPlayerCard(new Cards(CardsType.AIRLIFT));
+        OrderIssue.Commands = "airlift " + l_CountryList1.get(0).getCountryName() + " " + l_CountryList2.get(1).getCountryName()+" "+2;
+        Order l_Order1 = OrderCreator.CreateOrder(OrderIssue.Commands.split(" "), l_Player1);
+        l_Player1.receiveOrder(l_Order1);
+        assertFalse(l_Player1.nextOrder().validateCommand());
+    }
+
 }
+
